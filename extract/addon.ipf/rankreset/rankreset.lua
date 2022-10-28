57ff58d97151143c54bc28017e990c17c8e4e465
Impecide <moonstrike7723@gmail.com>
2022-10-28 18:21:52 -0400

    146932_001001.ipf

--- extract/addon.ipf/rankreset/rankreset.lua
+++ extract/addon.ipf/rankreset/rankreset.lua
@@ -1,6 +1,9 @@
 -- rankreset
+
+
 function RANKRESET_ON_INIT(addon, frame)
-	addon:RegisterOpenOnlyMsg('EQUIP_ITEM_LIST_GET', 'RANKRESET_PC_EQUIP_STATE');	
+	addon:RegisterOpenOnlyMsg('EQUIP_ITEM_LIST_GET', 'RANKRESET_PC_EQUIP_STATE');
+	addon:RegisterOpenOnlyMsg('RESET_ABILITY_UP', 'RANKRESET_PC_ABILITY_STATE');
 	addon:RegisterOpenOnlyMsg('AUTOSELLER_UPDATE', 'RANKRESET_PC_AUTOSELLER_STATE'); 
 end
 
@@ -20,46 +23,22 @@ function RANKRESET_ITEM_USE(invItem)
 		return;
 	end
 
-    local obj = GetIES(invItem:GetObject());
-    if obj.ClassName == '1706Event_RankReset' then
-        if session.GetPcTotalJobGrade() > 6 then
-          ui.SysMsg(ScpArgMsg('CantUseRankRest6Rank'));
-          return
-        end
-    end
-    RANKRESET_OPEN('RankReset', 'None', invItem);
-end
-
-local function _INIT_BY_MODE(frame, mode)
-	local str = GET_CHILD_RECURSIVELY(frame, 'str');
-	local endTime2 = GET_CHILD_RECURSIVELY(frame, 'endTime2');
-	
-	str:SetTextByKey('value', ClMsg('DoYouWant'..mode));
-	if mode == 'RankReset' then
-		endTime2:ShowWindow(1);
-	else
-		endTime2:ShowWindow(0);
-	end
-end
-
-function RANKRESET_OPEN(mode, targetCtrlType, invItem)
 	local frame = ui.GetFrame("rankreset");
-	frame:ShowWindow(1);
-	frame:SetUserValue('MODE', mode);
-	frame:SetUserValue('TARGET_CTRLTYPE', targetCtrlType);
-	if invItem ~= nil then
-		frame:SetUserValue("itemIES", invItem:GetIESID());
-	end
-	_INIT_BY_MODE(frame, mode);
-	RANKRESET_CHECK_PLAYER_STATE(frame);	
+	frame:ShowWindow(1)
+	frame:SetUserValue("itemIES", invItem:GetIESID());
+
+	RANKRESET_CHECK_PLAYER_STATE(frame);
 end
 
 function RANKRESET_CHECK_PLAYER_STATE(frame)
+
 	RANKRESET_PC_EQUIP_STATE(frame);
+	RANKRESET_PC_ABILITY_STATE(frame);
 	RANKRESET_PC_WITH_COMMPANION(frame);
 	RANKRESET_PC_LOCATE(frame);
 	RANKRESET_PC_AUTOSELLER_STATE(frame);
 	RANKRESET_PC_TIMEACTION_STATE(frame);
+	RANKRESET_PC_GUILDMASTER_STATE(frame);
 end
 
 function RANKRESET_PC_TIMEACTION_STATE(frame)
@@ -72,8 +51,7 @@ function RANKRESET_PC_TIMEACTION_STATE(frame)
 
 	local invframe = ui.GetFrame("inventory");
 	if invframe:GetUserValue('ITEM_GUID_IN_MORU') ~= 'None'
-		or 'None' ~= invframe:GetUserValue("ITEM_GUID_IN_TRANSCEND")
-		or 'None' ~= invframe:GetUserValue("ITEM_GUID_IN_TRANSCEND_SCROLL") then
+		or 'None' ~= invframe:GetUserValue("ITEM_GUID_IN_AWAKEN") then
 		nonstate = false;
 	end
 
@@ -82,6 +60,25 @@ function RANKRESET_PC_TIMEACTION_STATE(frame)
 		timeaction:SetCheck(1);
 	else
 		timeaction:SetCheck(0);
+	end
+end
+
+function RANKRESET_PC_GUILDMASTER_STATE(frame)
+	local isLeader = AM_I_LEADER(PARTY_GUILD);
+	if isLeader == 1 then
+		local templer = session.GetJobGrade(1016);
+		templer = tonumber(templer);
+		if templer == 0 then
+			isLeader = 0;
+		else
+			isLeader = 1;
+		end
+	end
+	local master_check = GET_CHILD(frame, 'master_check', "ui::CCheckBox");
+	if 0 == isLeader then
+		master_check:SetCheck(1);
+	else
+		master_check:SetCheck(0);
 	end
 end
 
@@ -117,9 +114,8 @@ end
 
 function RANKRESET_PC_WITH_COMMPANION(frame)
 	local summonedPet = GET_SUMMONED_PET();
-	local hawk = GET_SUMMONED_PET_HAWK();
 	local com_check = GET_CHILD(frame, 'com_check', "ui::CCheckBox");
-	if summonedPet == nil and hawk == nil then
+	if summonedPet == nil then
 		com_check:SetCheck(1);
 	else
 		com_check:SetCheck(0);
@@ -130,7 +126,7 @@ function RANKRESET_PC_EQUIP_STATE(frame)
 	local equipList = session.GetEquipItemList();
 	local unEquip = false;
 	for i = 0, equipList:Count() - 1 do
-		local equipItem = equipList:GetEquipItemByIndex(i);
+		local equipItem = equipList:Element(i);
 		local spotName = item.GetEquipSpotName(equipItem.equipSpot);	
 		if  equipItem.type  ~=  item.GetNoneItem(equipItem.equipSpot)  then
 			unEquip = true;
@@ -146,9 +142,34 @@ function RANKRESET_PC_EQUIP_STATE(frame)
 	end
 end
 
+function RANKRESET_PC_ABILITY_STATE(frame)
+	local pc = GetMyPCObject();
+	local runAbil = false;
+	for i = 0, RUN_ABIL_MAX_COUNT do
+		local prop = "None";
+		if 0 == i then
+			prop = "LearnAbilityID";
+		else
+			prop = "LearnAbilityID_" ..i;
+		end
+		if pc[prop] ~= nil and pc[prop] > 0 then
+			runAbil = true;
+			break;
+		end
+	end
+
+	local ability_check = GET_CHILD(frame, 'ability_check', "ui::CCheckBox");
+	if false == runAbil then
+		ability_check:SetCheck(1);
+	else
+		ability_check:SetCheck(0);
+	end
+end
+
 function RANKRESET_ITEM_USE_BUTTON_CLICK(frame, ctrl)
+
 	local gradeRank = session.GetPcTotalJobGrade();
-	if frame:GetUserValue('MODE') == 'RankReset' and gradeRank <= 1 then
+	if gradeRank <= 1 then
 		ui.SysMsg(ScpArgMsg("CantUseRankRest1Rank"));
 	    return;
 	end
@@ -159,58 +180,13 @@ function RANKRESET_ITEM_USE_BUTTON_CLICK(frame, ctrl)
         ui.SysMsg(ClMsg("AllowedInTown"));
         return;
     end
-     
-    if CHECK_INVENTORY_HAS_RANK_CARD() == true then
-        ui.MsgBox_NonNested(ClMsg('YouHaveRankCardReallyRankReset?'), 0x00000000, frame:GetName(), 'None', 'None');
-        return;
-    end
-    
-    RANKRESET_REQUEST_RANK_RESET();
-end
 
-function CHECK_INVENTORY_HAS_RANK_CARD()
-    -- n랭크 카드
-    for i = 2, JOB_CHANGE_MAX_RANK do
-        local rankCardName = 'jexpCard_UpRank'..i;
-        if session.GetInvItemByName(rankCardName) ~= nil then
-            return true;
-        end
-    end
-    
-    -- 클래스 경험치 복구 카드
-    if session.GetInvItemByName('jexpCard_RestoreLastRank') ~= nil then
-        return true;
-    end
-
-    return false;
-end
-
-function RANKRESET_REQUEST_RANK_RESET()
-    local frame = ui.GetFrame('rankreset');
 	local itemIES = frame:GetUserValue("itemIES");
-	if frame:GetUserValue('MODE') == 'RankReset' then
-		session.job.ReqRankReset(itemIES);
-	else
-		session.job.ReqCtrlTypeReset(frame:GetUserValue('TARGET_CTRLTYPE'));
-	end
+	packet.RankResetItemUse(itemIES);
 end
 
 function RANKRESET_CANCEL_BUTTON_CLICK(frame, ctrl)
 	frame = frame:GetTopParentFrame();
 	frame:SetUserValue("itemIES", 'None');
 	frame:ShowWindow(0);
-end
-
-function RANKRESET_DELETE_RANK_CARD(className)
-    local deleteItem = session.GetInvItemByName(className);
-    if deleteItem == nil then
-        return;
-    end
-    local deleteItemCls = GetClass('Item', className);
-    if deleteItemCls == nil then
-        return;
-    end
-    local yesScp = string.format('control.CustomCommand("TAKE_ITEM", %d)', deleteItemCls.ClassID);
-
-    ui.MsgBox(ClMsg('DeleteCardBecauseYourRankTooHigh'), yesScp, 'None');
 end
